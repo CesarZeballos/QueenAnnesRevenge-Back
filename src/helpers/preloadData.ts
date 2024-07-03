467567b0dce3e4db4d581e7e27a684c889144a4a
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-07-03 11:16:05 +0200

    creacion del preload a la mitad
    me parece que es demasiada informacion solo para probar, despues lo creare correctamente

---
+++ src/helpers/preloadData.ts
@@ -0,0 +1,59 @@
+import { RecipeState } from "../entities/Recipes";
+
+
+const preloadRecipes = [{
+    name: "032",
+    nickname: "origin",
+    vape: [
+        {name: "limon", size: 0.175},
+        {name: "pomelo", size: 0.3025},
+    ],
+    macerated: [
+        {name: "enebro", size: 20},
+        {name: "regaliz", size: 1.55},
+        {name: "cardamomo", size: 0.23},
+        {name: "coriandro", size: 8},
+        {name: "angelica", size: 1.55},
+        {name: "sal", size: 0.5}
+    ],
+    abvMacerated: 0.9,
+    time: 9,
+    abvGin: 0.4
+},{
+    name: "031",
+    nickname: "origin",
+    vape: [
+        {name: "limon", size: 0.175},
+        {name: "pomelo", size: 0.3025},
+    ],
+    macerated: [
+        {name: "enebro", size: 22},
+        {name: "regaliz", size: 1.55},
+        {name: "cardamomo", size: 0.23},
+        {name: "coriandro", size: 8},
+        {name: "angelica", size: 1.55},
+        {name: "sal", size: 0.5}
+    ],
+    abvMacerated: 0.9,
+    time: 9,
+    abvGin: 0.4
+},{
+    name: "030",
+    nickname: "origin",
+    vape: [
+        {name: "limon", size: 0.175},
+        {name: "pomelo", size: 0.3025},
+    ],
+    macerated: [
+        {name: "enebro", size: 21},
+        {name: "regaliz", size: 1.55},
+        {name: "cardamomo", size: 0.23},
+        {name: "coriandro", size: 8},
+        {name: "angelica", size: 1.55},
+        {name: "sal", size: 0.5}
+    ],
+    abvMacerated: 0.9,
+    time: 9,
+    abvGin: 0.4
+},
+]
