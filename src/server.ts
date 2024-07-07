e7cf80949f4749e570073d98e04635339816a52e
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-07-07 16:29:30 +0200

    configuracion del cors...

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,14 @@ import cors from "cors";
 
 const server = express();
 
+const corsOptions = {
+    origin: 'https://queen-annes-revengeii.vercel.app', 
+    optionsSuccessStatus: 200
+};
+
+server.use(cors(corsOptions))
 server.use(morgan("dev"))
 server.use(express.json())
-server.use(cors())
 server.use(router)
 
 export default server
